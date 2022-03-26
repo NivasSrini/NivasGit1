ae73a63faca35123fb082eb03969ab2d681e8bcb
NivasSrini <srininivas111.sn@gmail.com>
2022-03-26 15:53:32 +0530

    done

---
+++ src/com/Sample/SampleGit.java
@@ -0,0 +1,22 @@
+package com.Sample;
+
+public class SampleGit {
+	
+	private void sample() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		String a = "Nivas";
+		String reverse ="";
+		
+		for (int i = 0; i < a.length(); i++) {
+			
+			reverse=a.charAt(i)+reverse;
+			
+		}
+		System.out.println(reverse);
+	}
+	
+}
